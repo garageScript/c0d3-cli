d51eeada2424d2c4341c877378ab261da92f207b
Michael Dinh <michaeldinh1994@gmail.com>
2020-06-02 22:49:56 -0700

    replace url for cli

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,7 @@ export const CREDENTIALS_PATH = path.join(HOME, DIR, CREDENTIAL_FILE)
 
 export const HIDDEN_DIR = path.join(HOME, DIR)
 
-export const URL = 'http://localhost:3000/api/graphql'
+export const URL = 'https://c0d3.com/api/graphql'
 
 // Token from test account asynchronymouse
 export const DEBUG_TOKEN =
