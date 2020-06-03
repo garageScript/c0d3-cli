7084e3db061e9024450d32965f02b0474f9468cd
Michael Dinh <michaeldinh1994@gmail.com>
2020-06-02 22:52:03 -0700

    remove comment

--- src/util/git.ts
+++ src/util/git.ts
@@ -12,8 +12,6 @@ export const getDiffAgainstMaster = async (): Promise<DiffObject> => {
   const { current } = await git.branch()
   if (current === 'master') throw new Error(WRONG_BRANCH)
   console.log(`${CURRENT_BRANCH} ${current}\n`)
-
-  // const diff = await git.diff([`--color`, `master..${current}`])
   const diff = {
     display: await git.diff([`--color`, `master..${current}`]),
     db: await git.diff([`master..${current}`]),
