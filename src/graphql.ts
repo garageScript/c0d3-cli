44c6d4a39649fca4a8a5141a1d751aed5754afa8
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-10 13:32:45 +0400

    chore: Remove cliToken field

--- src/graphql.ts
+++ src/graphql.ts
@@ -28,13 +28,11 @@ export const POST_SUBMISSION = `
   mutation createSubmission(
     $lessonId: Int!
     $challengeId: Int!
-    $cliToken: String
     $diff: String!
   ) {
     createSubmission(
       lessonId: $lessonId
       challengeId: $challengeId
-      cliToken: $cliToken
       diff: $diff
     ) {
       id
